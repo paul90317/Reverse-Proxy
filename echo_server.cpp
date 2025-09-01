9e66e52922eec930c43f6b1318908dd5de5b946a
Paul Wu <paul2309825@gmail.com>
2025-09-02 00:18:12 +0800

    chore: add .clang-format

--- echo_server.cpp
+++ echo_server.cpp
@@ -1,102 +1,82 @@
+#include <array>
 #include <boost/asio.hpp>
 #include <iostream>
 #include <memory>
-#include <array>
 
 using namespace boost::asio;
 using ip::tcp;
 
-class Session : public std::enable_shared_from_this<Session>
-{
-public:
-    Session(tcp::socket socket)
-        : socket_(std::move(socket))
-    {
-    }
+class Session : public std::enable_shared_from_this<Session> {
+ public:
+  Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
-    void start()
-    {
-        do_read();
-    }
+  void start() { do_read(); }
 
-private:
-    void do_read()
-    {
-        auto self(shared_from_this());
-        socket_.async_read_some(buffer(data_),
-            [this, self](boost::system::error_code ec, std::size_t length)
-            {
-                if (!ec)
-                {
-                    do_write(length);
-                }
-            });
-    }
+ private:
+  void do_read() {
+    auto self(shared_from_this());
+    socket_.async_read_some(
+        buffer(data_),
+        [this, self](boost::system::error_code ec, std::size_t length) {
+          if (!ec) {
+            do_write(length);
+          }
+        });
+  }
 
-    void do_write(std::size_t length)
-    {
-        auto self(shared_from_this());
-        async_write(socket_, buffer(data_, length),
-            [this, self](boost::system::error_code ec, std::size_t /*length*/)
-            {
-                if (!ec)
-                {
-                    do_read();
-                }
-            });
-    }
+  void do_write(std::size_t length) {
+    auto self(shared_from_this());
+    async_write(
+        socket_, buffer(data_, length),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec) {
+            do_read();
+          }
+        });
+  }
 
-    tcp::socket socket_;
-    std::array<char, 1024> data_;
+  tcp::socket socket_;
+  std::array<char, 1024> data_;
 };
 
-class Server
-{
-public:
-    Server(io_context& io_context, u_short  port)
-        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
-    {
-        do_accept();
-    }
+class Server {
+ public:
+  Server(io_context &io_context, u_short port)
+      : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+    do_accept();
+  }
 
-private:
-    void do_accept()
-    {
-        acceptor_.async_accept(
-            [this](boost::system::error_code ec, tcp::socket socket)
-            {
-                if(ec) {
-                    return;
-                }
-                std::cout << "New client" << std::endl;
-                std::make_shared<Session>(std::move(socket))->start();
-                do_accept();
-            });
-    }
+ private:
+  void do_accept() {
+    acceptor_.async_accept(
+        [this](boost::system::error_code ec, tcp::socket socket) {
+          if (ec) {
+            return;
+          }
+          std::cout << "New client" << std::endl;
+          std::make_shared<Session>(std::move(socket))->start();
+          do_accept();
+        });
+  }
 
-    tcp::acceptor acceptor_;
+  tcp::acceptor acceptor_;
 };
 
-int main(int argc, char* argv[])
-{
-    try
-    {
-        if (argc != 2)
-        {
-            std::cerr << "Usage: " << argv[0] << " <port>\n";
-            return 1;
-        }
-
-        io_context io_context;
-
-        Server s(io_context, std::atoi(argv[1]));
-        std::cout << "Server started on port " << argv[1] << std::endl;
-        io_context.run();
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
+int main(int argc, char *argv[]) {
+  try {
+    if (argc != 2) {
+      std::cerr << "Usage: " << argv[0] << " <port>\n";
+      return 1;
     }
 
-    return 0;
+    io_context io_context;
+
+    Server s(io_context, std::atoi(argv[1]));
+    std::cout << "Server started on port " << argv[1] << std::endl;
+    io_context.run();
+  } catch (std::exception &e) {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }
