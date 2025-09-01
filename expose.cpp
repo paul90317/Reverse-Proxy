2d6ae2e78ca6eeb716bb851b9145ed88ff2b7593
Paul Wu <paul2309825@gmail.com>
2025-09-02 00:18:12 +0800

    chore: add .clang-format

--- expose.cpp
+++ expose.cpp
@@ -10,183 +10,169 @@ tcp::resolver resolver(context);
 
 // Arguments
 std::string proxy_server_ip;
-u_short  ctrl_port;
-u_short  proxy_port;
-u_short  target_port;
+u_short ctrl_port;
+u_short proxy_port;
+u_short target_port;
 
 tcp::endpoint target_endpoint;
 
-class Session : public std::enable_shared_from_this<Session>
-{
-public:
-    Session(u_short  _agent_port)
-        : agent_port(_agent_port), proxy(context), target(context)
-    {
-        
-    }
+class Session : public std::enable_shared_from_this<Session> {
+ public:
+  Session(u_short _agent_port)
+      : agent_port(_agent_port), proxy(context), target(context) {}
 
-    void do_bridge()
-    {
-        auto self(shared_from_this());
-        resolver.async_resolve(tcp::v4(), proxy_server_ip, std::to_string(agent_port), [this, self](const boost::system::error_code &ec, tcp::resolver::results_type results)
-        {
-            if (ec)
-            {
-                std::cout << "Proxy server not found" << std::endl;
-                return;
-            }
-            async_connect(proxy, results, [this, self](const boost::system::error_code &ec, const tcp::endpoint)
-            {
-                if (ec)
-                {
-                    std::cout << "Proxy connection failed" << std::endl;
-                    return;
+  void do_bridge() {
+    auto self(shared_from_this());
+    resolver.async_resolve(
+        tcp::v4(), proxy_server_ip, std::to_string(agent_port),
+        [this, self](const boost::system::error_code &ec,
+                     tcp::resolver::results_type results) {
+          if (ec) {
+            std::cout << "Proxy server not found" << std::endl;
+            return;
+          }
+          async_connect(
+              proxy, results,
+              [this, self](const boost::system::error_code &ec,
+                           const tcp::endpoint) {
+                if (ec) {
+                  std::cout << "Proxy connection failed" << std::endl;
+                  return;
                 }
-                target.async_connect(target_endpoint, [this, self](const boost::system::error_code &ec)
-                {
-                    if (ec)
-                    {
+                target.async_connect(
+                    target_endpoint,
+                    [this, self](const boost::system::error_code &ec) {
+                      if (ec) {
                         std::cout << "Target connection failed" << std::endl;
                         return;
-                    }
-                    std::cout << "Connection created" << std::endl;
-                    auto piper = std::make_shared<depipe>(std::move(proxy), std::move(target));
-                    piper->pipe();
-                    piper->rpipe();
-                });
-            });
+                      }
+                      std::cout << "Connection created" << std::endl;
+                      auto piper = std::make_shared<depipe>(std::move(proxy),
+                                                            std::move(target));
+                      piper->pipe();
+                      piper->rpipe();
+                    });
+              });
         });
-    }
+  }
 
-private:
-    u_short  agent_port;
-    std::array<char, 2> buf;
-    tcp::socket proxy;
-    tcp::socket target;
+ private:
+  u_short agent_port;
+  std::array<char, 2> buf;
+  tcp::socket proxy;
+  tcp::socket target;
 };
 
-class Agent : public std::enable_shared_from_this<Agent>
-{
-public:
-    Agent(tcp::socket _control)
-        : control(std::move(_control))
-    {
-    }
+class Agent : public std::enable_shared_from_this<Agent> {
+ public:
+  Agent(tcp::socket _control) : control(std::move(_control)) {}
 
-    void do_request()
-    {
-        auto self(shared_from_this());
-        resolver.async_resolve(tcp::v4(), proxy_server_ip, std::to_string(ctrl_port), [this, self](const boost::system::error_code &ec, tcp::resolver::results_type results)
-        {
-            if (ec)
-            {
-                std::cout << "Proxy server not found" << std::endl;
-                return;
-            }
-            async_connect(control, results, [this, self](const boost::system::error_code &ec, const tcp::endpoint)
-            {
-                if (ec)
-                {
-                    std::cout << "Proxy server not found" << std::endl;
-                    return;
-                }
+  void do_request() {
+    auto self(shared_from_this());
+    resolver.async_resolve(
+        tcp::v4(), proxy_server_ip, std::to_string(ctrl_port),
+        [this, self](const boost::system::error_code &ec,
+                     tcp::resolver::results_type results) {
+          if (ec) {
+            std::cout << "Proxy server not found" << std::endl;
+            return;
+          }
+          async_connect(control, results,
+                        [this, self](const boost::system::error_code &ec,
+                                     const tcp::endpoint) {
+                          if (ec) {
+                            std::cout << "Proxy server not found" << std::endl;
+                            return;
+                          }
 
-                memcpy(buf.data(), &proxy_port, 2);
-                std::swap(buf[0], buf[1]);
-                async_write(control, buffer(buf), [this, self](const boost::system::error_code &ec, size_t bytes_read)
-                {
-                    if (ec)
-                    {
-                        std::cout << "Proxy failed" << std::endl;
-                        return;
-                    }
-                    std::cout << "Proxy at " << proxy_server_ip << ":" << proxy_port << std::endl;
-                    do_handle_connection();
-                });
-            });
+                          memcpy(buf.data(), &proxy_port, 2);
+                          std::swap(buf[0], buf[1]);
+                          async_write(
+                              control, buffer(buf),
+                              [this, self](const boost::system::error_code &ec,
+                                           size_t bytes_read) {
+                                if (ec) {
+                                  std::cout << "Proxy failed" << std::endl;
+                                  return;
+                                }
+                                std::cout << "Proxy at " << proxy_server_ip
+                                          << ":" << proxy_port << std::endl;
+                                do_handle_connection();
+                              });
+                        });
         });
-    }
-private:
-    void do_handle_connection()
-    {
-        auto self(shared_from_this());
-        async_read(control, buffer(buf), [this, self](const boost::system::error_code &ec, size_t bytes_read)
-        {
-            if (ec)
-            {
-                return;
-            }
-            std::swap(buf[0], buf[1]);
-            u_short  port = *(u_short  *)buf.data();
-            std::cout << "New random port at " << port << std::endl;
-            std::make_shared<Session>(port)->do_bridge();
-            do_handle_connection();
-        });
-    }
+  }
 
-private:
-    std::array<char, 2> buf;
-    tcp::socket control;
+ private:
+  void do_handle_connection() {
+    auto self(shared_from_this());
+    async_read(
+        control, buffer(buf),
+        [this, self](const boost::system::error_code &ec, size_t bytes_read) {
+          if (ec) {
+            return;
+          }
+          std::swap(buf[0], buf[1]);
+          u_short port = *(u_short *)buf.data();
+          std::cout << "New random port at " << port << std::endl;
+          std::make_shared<Session>(port)->do_bridge();
+          do_handle_connection();
+        });
+  }
+
+ private:
+  std::array<char, 2> buf;
+  tcp::socket control;
 };
 
-int main(int argc, char *argv[])
-{
-    char *ptr =  std::getenv("PROXY_HOST");
-    if (ptr == nullptr)
-    {
-        std::cerr << "Environment variable PROXY_HOST not set\n";
-        return 1;
+int main(int argc, char *argv[]) {
+  char *ptr = std::getenv("PROXY_HOST");
+  if (ptr == nullptr) {
+    std::cerr << "Environment variable PROXY_HOST not set\n";
+    return 1;
+  }
+  try {
+    std::string proxy_server_link(ptr);
+    size_t delimiter = proxy_server_link.find(':');
+    if (delimiter == std::string::npos) {
+      std::cerr << "Environment variable PROXY_HOST should follow the format "
+                   "<server_ip>:<server_port>\n";
+      return 1;
     }
-    try {
-        
-        std::string proxy_server_link(ptr);
-        size_t delimiter = proxy_server_link.find(':');
-        if (delimiter == std::string::npos)
-        {
-            std::cerr << "Environment variable PROXY_HOST should follow the format <server_ip>:<server_port>\n";   
-            return 1;
-        }
-        proxy_server_ip = proxy_server_link.substr(0, delimiter);
-        ctrl_port = std::stoi(proxy_server_link.substr(delimiter + 1));
-    } catch (...) {
-        std::cerr << "Environment variable PROXY_HOST should follow the format <server_ip>:<server_port>\n";   
-            return 1;
-    }
+    proxy_server_ip = proxy_server_link.substr(0, delimiter);
+    ctrl_port = std::stoi(proxy_server_link.substr(delimiter + 1));
+  } catch (...) {
+    std::cerr << "Environment variable PROXY_HOST should follow the format "
+                 "<server_ip>:<server_port>\n";
+    return 1;
+  }
 
-    if (argc != 2)
-    {
-        std::cerr << "Usage: expose <proxy_port>:<target_port>\n";
-        return 1;
-    }
+  if (argc != 2) {
+    std::cerr << "Usage: expose <proxy_port>:<target_port>\n";
+    return 1;
+  }
 
-    try
-    {
-        std::string proxy_target = argv[1];
-        size_t delimiter = proxy_target.find(':');
-        if (delimiter == std::string::npos)
-        {
-            std::cerr << "Usage: expose <proxy_port>:<target_port>\n";
-            return 1;
-        }
-        proxy_port = std::stoi(proxy_target.substr(0, delimiter));
-        target_port = std::stoi(proxy_target.substr(delimiter + 1));
+  try {
+    std::string proxy_target = argv[1];
+    size_t delimiter = proxy_target.find(':');
+    if (delimiter == std::string::npos) {
+      std::cerr << "Usage: expose <proxy_port>:<target_port>\n";
+      return 1;
     }
-    catch (...)
-    {
-        std::cerr << "Usage: expose <proxy_port>:<target_port>\n";
-        return 1;
-    }
+    proxy_port = std::stoi(proxy_target.substr(0, delimiter));
+    target_port = std::stoi(proxy_target.substr(delimiter + 1));
+  } catch (...) {
+    std::cerr << "Usage: expose <proxy_port>:<target_port>\n";
+    return 1;
+  }
 
-    try
-    {
-        target_endpoint = tcp::endpoint(ip::make_address("127.0.0.1"), target_port);
-        std::make_shared<Agent>(tcp::socket(context))->do_request();
-        context.run();
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << "Agent error: " << e.what() << std::endl;
-    }
+  try {
+    target_endpoint = tcp::endpoint(ip::make_address("127.0.0.1"), target_port);
+    std::make_shared<Agent>(tcp::socket(context))->do_request();
+    context.run();
+  } catch (std::exception &e) {
+    std::cerr << "Agent error: " << e.what() << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
--- proxy_server.cpp
+++ proxy_server.cpp
@@ -8,173 +8,156 @@ using namespace boost::asio;
 using ip::tcp;
 io_context context;
 
-class Session : public std::enable_shared_from_this<Session>
-{
-public:
-    Session(tcp::socket *_control, tcp::socket _client)
-        : control(_control), client(std::move(_client)), agent_acceptor(context)
-    {
-    }
-    void do_connect_agent()
-    {
-        auto self(shared_from_this());
+class Session : public std::enable_shared_from_this<Session> {
+ public:
+  Session(tcp::socket *_control, tcp::socket _client)
+      : control(_control),
+        client(std::move(_client)),
+        agent_acceptor(context) {}
+  void do_connect_agent() {
+    auto self(shared_from_this());
 
-        tcp::endpoint endpoint(tcp::v4(), 0);
-        agent_acceptor.open(endpoint.protocol());
-        agent_acceptor.set_option(tcp::acceptor::reuse_address(false));
-        agent_acceptor.bind(endpoint);
-        agent_acceptor.listen();
+    tcp::endpoint endpoint(tcp::v4(), 0);
+    agent_acceptor.open(endpoint.protocol());
+    agent_acceptor.set_option(tcp::acceptor::reuse_address(false));
+    agent_acceptor.bind(endpoint);
+    agent_acceptor.listen();
 
-        u_short port = agent_acceptor.local_endpoint().port();
-        memcpy(buf.data(), &port, 2);
-        std::cout << "New random port at " << port << std::endl;
-        std::swap(buf[0], buf[1]);
-        async_write(*control, buffer(buf), [this, self](const boost::system::error_code &ec, size_t size)
-                    {
-            if (ec || size != 2) {
-                return;
+    u_short port = agent_acceptor.local_endpoint().port();
+    memcpy(buf.data(), &port, 2);
+    std::cout << "New random port at " << port << std::endl;
+    std::swap(buf[0], buf[1]);
+    async_write(
+        *control, buffer(buf),
+        [this, self](const boost::system::error_code &ec, size_t size) {
+          if (ec || size != 2) {
+            return;
+          }
+
+          agent_acceptor.async_accept([this, self](boost::system::error_code ec,
+                                                   tcp::socket agent) {
+            if (ec) {
+              return;
             }
-            
-            agent_acceptor.async_accept([this, self](boost::system::error_code ec, tcp::socket agent)
-                         {
-                if (ec) {
-                    return;
-                }
-                agent_acceptor.close();
-                std::cout << "Connection created" << std::endl;
-                auto piper = std::make_shared<depipe>(std::move(client), std::move(agent));
-                piper->pipe();
-                piper->rpipe();
-            }); });
-    }
+            agent_acceptor.close();
+            std::cout << "Connection created" << std::endl;
+            auto piper =
+                std::make_shared<depipe>(std::move(client), std::move(agent));
+            piper->pipe();
+            piper->rpipe();
+          });
+        });
+  }
 
-private:
-    std::array<char, 2> buf;
-    tcp::socket *control;
-    tcp::socket client;
-    tcp::acceptor agent_acceptor;
+ private:
+  std::array<char, 2> buf;
+  tcp::socket *control;
+  tcp::socket client;
+  tcp::acceptor agent_acceptor;
 };
 
-class Agent : public std::enable_shared_from_this<Agent>
-{
-public:
-    Agent(tcp::socket _control)
-        : control(std::move(_control)), proxy(context)
-    {
-    }
-    void do_proxy()
-    {
-        auto self(shared_from_this());
-        async_read(control, buffer(buf), [this, self](const boost::system::error_code &ec, size_t size)
-                   {
-            if (ec || size != 2) {
-                return;
-            }
-            std::swap(buf[0], buf[1]);
-            port = * (u_short  *) buf.data();
-            std::cout << "New proxy request the port " << port << std::endl;
-            tcp::endpoint endpoint(tcp::v4(), port);
-            proxy.open(endpoint.protocol());
-            proxy.set_option(tcp::acceptor::reuse_address(false));
-            try {
-                proxy.bind(endpoint);
-            } catch (...) {
-                std::cerr << "Failed to bind to port " << port << std::endl;
-                return;
-            }
-            proxy.listen(); 
-            do_accept(); 
-            do_close(); });
-    }
+class Agent : public std::enable_shared_from_this<Agent> {
+ public:
+  Agent(tcp::socket _control) : control(std::move(_control)), proxy(context) {}
+  void do_proxy() {
+    auto self(shared_from_this());
+    async_read(control, buffer(buf),
+               [this, self](const boost::system::error_code &ec, size_t size) {
+                 if (ec || size != 2) {
+                   return;
+                 }
+                 std::swap(buf[0], buf[1]);
+                 port = *(u_short *)buf.data();
+                 std::cout << "New proxy request the port " << port
+                           << std::endl;
+                 tcp::endpoint endpoint(tcp::v4(), port);
+                 proxy.open(endpoint.protocol());
+                 proxy.set_option(tcp::acceptor::reuse_address(false));
+                 try {
+                   proxy.bind(endpoint);
+                 } catch (...) {
+                   std::cerr << "Failed to bind to port " << port << std::endl;
+                   return;
+                 }
+                 proxy.listen();
+                 do_accept();
+                 do_close();
+               });
+  }
 
-private:
-    void do_close()
-    {
-        control.async_read_some(buffer(buf), [this](const boost::system::error_code &ec, size_t size)
-                                {
-            proxy.close();
-            control.close(); });
-    }
-    void do_accept()
-    {
-        auto self(shared_from_this());
-        proxy.async_accept(
-            [this, self](boost::system::error_code ec, tcp::socket client)
-            {
-                if (ec)
-                {
-                    std::cerr << "Proxy closed at " << port << std::endl;
-                    return;
-                }
-                std::cout << "New connection at " << proxy.local_endpoint().port() << std::endl;
-                std::make_shared<Session>(&control, std::move(client))->do_connect_agent();
-                do_accept();
-            });
-    }
+ private:
+  void do_close() {
+    control.async_read_some(
+        buffer(buf), [this](const boost::system::error_code &ec, size_t size) {
+          proxy.close();
+          control.close();
+        });
+  }
+  void do_accept() {
+    auto self(shared_from_this());
+    proxy.async_accept(
+        [this, self](boost::system::error_code ec, tcp::socket client) {
+          if (ec) {
+            std::cerr << "Proxy closed at " << port << std::endl;
+            return;
+          }
+          std::cout << "New connection at " << proxy.local_endpoint().port()
+                    << std::endl;
+          std::make_shared<Session>(&control, std::move(client))
+              ->do_connect_agent();
+          do_accept();
+        });
+  }
 
-    std::array<char, 2> buf;
-    tcp::socket control;
-    tcp::acceptor proxy;
-    u_short  port;
+  std::array<char, 2> buf;
+  tcp::socket control;
+  tcp::acceptor proxy;
+  u_short port;
 };
 
-class Server : public std::enable_shared_from_this<Server>
-{
-public:
-    Server(u_short port)
-        : acceptor(context)
-    {
-        tcp::endpoint endpoint(tcp::v4(), port);
-        acceptor.open(endpoint.protocol());
-        acceptor.set_option(tcp::acceptor::reuse_address(false));
-        acceptor.bind(endpoint);
-        acceptor.listen();
-    }
-    void do_accept()
-    {
-        auto self(shared_from_this());
-        acceptor.async_accept(
-            [this, self](boost::system::error_code ec, tcp::socket agent)
-            {
-                if (ec)
-                    return;
-                std::make_shared<Agent>(std::move(agent))->do_proxy();
-                do_accept();
-            });
-    }
+class Server : public std::enable_shared_from_this<Server> {
+ public:
+  Server(u_short port) : acceptor(context) {
+    tcp::endpoint endpoint(tcp::v4(), port);
+    acceptor.open(endpoint.protocol());
+    acceptor.set_option(tcp::acceptor::reuse_address(false));
+    acceptor.bind(endpoint);
+    acceptor.listen();
+  }
+  void do_accept() {
+    auto self(shared_from_this());
+    acceptor.async_accept(
+        [this, self](boost::system::error_code ec, tcp::socket agent) {
+          if (ec) return;
+          std::make_shared<Agent>(std::move(agent))->do_proxy();
+          do_accept();
+        });
+  }
 
-private:
-    tcp::acceptor acceptor;
+ private:
+  tcp::acceptor acceptor;
 };
 
-int main(int argc, char *argv[])
-{
-    u_short control_port;
-    if (argc != 2)
-    {
-        std::cerr << "Usage: proxy_server <control_port>\n";
-        return 1;
-    }
-    try
-    {
-        control_port = std::stoi(argv[1]);
-    }
-    catch (...)
-    {
-        std::cerr << "Usage: proxy_server <control_port>\n";
-        return 1;
-    }
-    try
-    {
-        std::make_shared<Server>(control_port)->do_accept();
-        std::cout << "Server started on port " << control_port << std::endl;
-        context.run();
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << "Agent error: " << e.what() << std::endl;
-        return 1;
-    }
+int main(int argc, char *argv[]) {
+  u_short control_port;
+  if (argc != 2) {
+    std::cerr << "Usage: proxy_server <control_port>\n";
+    return 1;
+  }
+  try {
+    control_port = std::stoi(argv[1]);
+  } catch (...) {
+    std::cerr << "Usage: proxy_server <control_port>\n";
+    return 1;
+  }
+  try {
+    std::make_shared<Server>(control_port)->do_accept();
+    std::cout << "Server started on port " << control_port << std::endl;
+    context.run();
+  } catch (std::exception &e) {
+    std::cerr << "Agent error: " << e.what() << std::endl;
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }
